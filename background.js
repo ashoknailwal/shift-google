e7882b87f35e38b3ba2040589c2e0374a3cc2606
ashok <ashoknailwal@gmail.com>
2017-07-20 10:44:39 +0530

    improved functionality

--- background.js
+++ background.js
@@ -3,8 +3,12 @@ chrome.runtime.onMessage.addListener(
         //if(request.name === "Shift Google"){
             //chrome.extension.getBackgroundPage().console.log('resp.type');
             //console.log("Message recieved");
+            var str = request.searchText;
+            if(str != null && str.length != 0){
+            	console.log(str.length);
             var query = 'http://google.com/search?q='+request.searchText;
             chrome.tabs.create({url:query});
+        }
             sendResponse({message: ""});
         //}
     }
